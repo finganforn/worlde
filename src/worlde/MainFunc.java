c4777e56e07332d2c2854bdd7e2006123ead55db
User <User@LAPTOP-056M0SH8.mynet>
2022-12-26 06:49:45 +0100

    first commit

---
+++ src/worlde/MainFunc.java
@@ -0,0 +1,485 @@
+package worlde;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MainFunc {
+	
+	static String swedishAlphabet = "QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM";
+	static String englishAlphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	static String swedishShortcut = "QWRUSDFHJÄZXCB";
+	static String englishShortcut = "QGJZXVB chuff";
+	static String swedishFile = "C:\\Users\\User\\eclipse-workspace\\worlde\\src\\worlde\\svenska-ord.txt";
+	static String englishFile = "C:\\Users\\User\\eclipse-workspace\\worlde\\src\\worlde\\english-words.txt";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		int startP = random.nextInt(10000);
+		//startP = 1950;
+		for (int i = startP; i < startP+100; i++) {
+			Stack<Integer> st = fac(i);
+			String s2 = "" + i + " = " + st.get(0);
+			for (int j = 1; j < st.size(); j++) {
+				s2 = s2 + " * " + st.get(j);
+			}
+			
+		}
+		
+		JFrame frame = new JFrame("WordleCheater");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 500);
+
+		JPanel topPanel = new JPanel();
+		JPanel midPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
+		JLabel ord = new JLabel("Ord");
+		JTextField ordBox = new JTextField("ENIGA");
+        JLabel letters = new JLabel("Bokstäver");
+        JTextField letterBox = new JTextField("QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM");
+        JLabel position = new JLabel("WrongPlace");
+        JTextField positionBox = new JTextField("DELETE");
+        JLabel wildCardLabel = new JLabel("somewhere");
+        JTextField wildCardBox = new JTextField("DELETE");
+        
+        JCheckBox englishBox = new JCheckBox("engelska");
+        
+        ArrayList<String> swedishWords = new ArrayList<String>();
+        ArrayList<String> swedishWords6 = new ArrayList<String>();
+        ArrayList<String> englishWords = new ArrayList<String>();
+        Scanner sc;
+        try {
+			sc = new Scanner(new File(swedishFile));
+			while(sc.hasNext()){
+				
+	            String s = sc.next().toUpperCase();
+	            s = s.replace("Ã¥", "Å");
+	            s= s.replace("Ã¤", "Ä");
+	            s = s.replace("Ã¶", "Ö");
+	            if (s.length() == 6)
+	            	swedishWords6.add(s);
+	            if (s.length() == 5) {
+	            	swedishWords.add(s);
+	            	
+	            	swedishWords6.add((s+"A"));
+	            	swedishWords6.add((s+"E"));
+	            	
+	            }
+	            if (s.length() == 4) {
+	            	swedishWords.add((s+"A"));
+	            	swedishWords.add((s+"E"));
+	            	
+	            	swedishWords6.add((s+"EN"));
+	            	swedishWords6.add((s+"ER"));
+	            	swedishWords6.add((s+"AR"));
+	            	swedishWords6.add((s+"AN"));
+	            	swedishWords6.add((s+"ET"));
+	            	swedishWords6.add((s+"AT"));
+	            	swedishWords6.add((s+"OR"));
+	            }
+	            if (s.length() == 3) {
+	            	swedishWords.add((s+"EN"));
+	            	swedishWords.add((s+"ER"));
+	            	swedishWords.add((s+"AR"));
+	            	swedishWords.add((s+"AN"));
+	            	swedishWords.add((s+"ET"));
+	            	swedishWords.add((s+"AT"));
+	            	swedishWords.add((s+"OR"));
+	            }
+	            
+				}
+			sc = new Scanner(new File(englishFile));
+			while(sc.hasNext()){
+				
+	            String s = sc.next();
+	            if (s.length() == 5)
+	            	englishWords.add(s.toUpperCase());
+	            if (s.length() == 4) {
+	            	englishWords.add((s+"s").toUpperCase());
+	            }
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+            //.....
+        
+
+
+
+        JButton send = new JButton("Generera ord");
+        JButton reset = new JButton("Återställ alfabet");
+        JButton shortReset = new JButton("litet alfabet");
+        JButton semantle = new JButton("sem");
+        JButton ordiligKnapp = new JButton("lig");
+
+        //JButton reset = new JButton("Reset");
+        topPanel.add(ord);
+        topPanel.add(ordBox);
+        topPanel.add(position);
+        topPanel.add(positionBox);
+        topPanel.add(wildCardLabel);
+        topPanel.add(wildCardBox);
+        midPanel.add(letters);
+        midPanel.add(letterBox);
+        midPanel.add(englishBox);
+        midPanel.add(reset);
+        midPanel.add(shortReset);
+        midPanel.add(semantle);
+        midPanel.add(ordiligKnapp);
+        //midPanel.add(publishAdvice);
+        //midPanel.add(everythingBox);
+        JPanel sendPanel = new JPanel();
+        
+        sendPanel.add(send);
+        
+        JPanel progressInfoPanel = new JPanel();
+
+
+        bottomPanel.add(BorderLayout.NORTH, sendPanel);
+        bottomPanel.add(BorderLayout.SOUTH, progressInfoPanel);
+
+        // Text Area at the Center
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.NORTH, topPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, midPanel);        
+        frame.getContentPane().add(BorderLayout.SOUTH, bottomPanel);
+        frame.setVisible(true);
+        
+
+        reset.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+				boolean english = englishBox.isSelected();
+				letterBox.setText(english ? englishAlphabet : swedishAlphabet);
+			}
+        	
+        });
+        
+        semantle.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+				double similarity = 0;
+				int index1 = 0;
+				int index2 = 0;
+				int index3 = 0;
+				int index4 = 0;
+				int index5 = 0;
+				for (int i = 0; i < englishWords.size(); i++) {
+					double thisSim = Wordle.similarity(letterBox.getText(), englishWords.get(i));
+					if (thisSim > similarity) {
+						index5 = 4;
+						index4 = index3;
+						index3 = index2;
+						index2 = index1;
+						index1 = i;
+						similarity = thisSim;
+					}
+						
+				}
+				letterBox.setText(letterBox.getText() + " is similar to: " + 
+						englishWords.get(index1)  + " " + 
+						englishWords.get(index2) + " " + 
+						englishWords.get(index3) + " " + 
+						englishWords.get(index4) + " " + 
+						englishWords.get(index5));
+			}
+        	
+        });
+        
+        shortReset.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+				boolean english = englishBox.isSelected();
+				letterBox.setText(english ? englishShortcut : swedishShortcut);
+			}
+        	
+        });
+        ordiligKnapp.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+				letterBox.setText("QWYOPHJÖÄZXCVB");
+			}
+        	
+        });
+        
+        send.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+				String word = ordBox.getText().toUpperCase();
+				String letters = letterBox.getText().toUpperCase();
+				String wildcards = wildCardBox.getText().toUpperCase();
+				String positions = positionBox.getText().toUpperCase();
+				ArrayList<Character> allowed = new ArrayList<Character>();
+				ArrayList<Character> required = new ArrayList<Character>();
+				ArrayList<Integer> wrongPos = new ArrayList<Integer>();
+				ArrayList<Character> wildCards = new ArrayList<Character>();
+				//System.out.println(word + " " + letters + " " + positions);
+				for (int i = 0; i < letters.length(); i++)   {
+					char c = letters.charAt(i);
+					if (Character.isLetter(c))
+						allowed.add(letters.charAt(i));
+				}
+				for (int i = 0; i < wildcards.length(); i++) {
+					char c = wildcards.charAt(i);
+					if (Character.isLetter(c))
+						wildCards.add(wildcards.charAt(i));
+						
+				}
+				String[] posSplit = positions.split(" ");
+
+				if (posSplit[0].length() > 1) {
+				for (int i = 0; i < posSplit.length; i++) {				
+						
+						String num = posSplit[i].substring(1);
+						try {
+						int iNum = Integer.parseInt(num);
+						char let = posSplit[i].charAt(0);
+						if (iNum > 0) {
+							required.add(let);
+							//wrongPos.add(iNum); ENKLARE INDEX
+							wrongPos.add(iNum-1);
+						}
+						}
+						catch (NumberFormatException ex) {
+							System.out.println("ignoring " + posSplit[i]);
+						}
+					}
+				}
+				
+				//avbryt om för många alternativ
+				int solutions = solutionsCount(word, allowed, required, wildCards);
+				System.out.println(solutions + " possible words");
+				if (solutions > 300000)
+					JOptionPane.showMessageDialog(null, "too many solutions ("+ solutions + ")");	
+				
+				else {
+					ArrayList<String> ordelRes = Wordle.ordel(word, allowed, required, wrongPos, wildCards);
+					
+					String allWords = "";
+					for (int i = 0; i < ordelRes.size(); i++) {
+						
+						if (englishBox.isSelected()) {
+							if (englishWords.contains(ordelRes.get(i))) {
+								System.out.println(ordelRes.get(i));
+								allWords += ordelRes.get(i) + " ";
+							}
+							
+						}
+						else {
+							if (word.length() == 6) {
+								if (swedishWords6.contains(ordelRes.get(i))) {
+									System.out.println(ordelRes.get(i));
+									allWords += ordelRes.get(i) + " ";
+								}
+							}
+							else {
+							
+								if (swedishWords.contains(ordelRes.get(i))) {
+									System.out.println(ordelRes.get(i));
+									allWords += ordelRes.get(i) + " ";
+								}
+							}
+						}
+						
+					
+					}
+					JOptionPane.showMessageDialog(null, allWords);	
+				}
+				
+			}
+			
+
+			public void actionPerformed1(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("func2");
+			}
+        });
+
+
+	ArrayList<Character> allowed = new ArrayList<Character>();
+	allowed.add('Q'); 
+	allowed.add('W');
+	//allowed.add('E'); 
+	//allowed.add('R');
+	//allowed.add('T');
+	allowed.add('Y');
+	allowed.add('U');
+	//allowed.add('I');
+	//allowed.add('O');
+	allowed.add('P');
+	//allowed.add('Å');
+	//allowed.add('A');
+	//allowed.add('S');
+	//allowed.add('D');
+	//allowed.add('F');
+	//allowed.add('G');
+	//allowed.add('H');
+	allowed.add('J');
+	//allowed.add('K');
+	//allowed.add('L');
+	//allowed.add('Ö');
+	//allowed.add('Ä');
+	allowed.add('Z');
+	allowed.add('X');
+	allowed.add('C');
+	allowed.add('V');
+	//allowed.add('B');
+	//allowed.add('N');
+	allowed.add('M');
+	ArrayList<Character> required = new ArrayList<Character>();
+	ArrayList<Integer> requiredNonIndices = new ArrayList<Integer>();
+	
+
+
+
+	required.add('U');
+	requiredNonIndices.add(2);
+
+	required.add('U');
+	requiredNonIndices.add(3);
+
+	required.add('N');
+	requiredNonIndices.add(4);
+
+
+	
+
+
+	String availableLetters = "";
+	String currentWord = "";
+	ArrayList<String> wrongPlacements = new ArrayList<String>();
+		
+	
+	//ArrayList<String> ordel = Wordle.ordel("   CH", allowed, required, (ArrayList<Integer>) requiredNonIndices);
+	
+	//for (int i = 0; i < ordel.size(); i++)
+		//System.out.println(ordel.get(i));
+	}
+	
+	private static int solutionsCount(String word, ArrayList<Character> allowedChars, ArrayList<Character> requiredChars, ArrayList<Character> wildcards) {
+		int empty = 0;
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == ' ')
+				empty++;
+		}
+		ArrayList<Character> allLetters = new ArrayList<Character>();
+		
+		
+		for (Character c : allowedChars) {
+			if (!allLetters.contains(c))
+				allLetters.add(c);
+		}
+		for (Character c : requiredChars) {
+			if (!allLetters.contains(c))
+				allLetters.add(c);
+		}
+		for (Character c : wildcards) {
+			if (!allLetters.contains(c))
+				allLetters.add(c);
+		}
+		
+		
+		int solutions = (int) Math.pow(allowedChars.size(), empty);
+		return solutions;
+	}
+	
+	private static boolean prim(int tal) {
+		if (tal < 2)
+			return false;
+		for (int i = 2; i < (tal/2)+1; i++) {
+			if (tal % i == 0)
+				return false;
+		}
+		return true;
+	}
+	private static Stack<Integer> fac(int tal) {
+		Stack<Integer> res = new Stack<Integer>();
+		
+		res.add(tal);
+		boolean done = false;
+		
+		while (!done) {
+			System.out.print(facStackToString(res));
+			if (res.size() > 0)
+				System.out.print(" = ");
+			int last = res.pop();
+			if (last < 2) {
+				res.add(last);
+				System.out.println();
+				return res;
+			}
+			if (prim(last)) {
+				res.add(last);
+				System.out.println();
+				return res;
+			}
+			int low = lowestDiv(last);
+			res.add(low);
+			res.add(last / low);
+		}
+		
+		System.out.println();
+		return res;
+		
+	}
+	private static String facStackToString(Stack<Integer> st) {
+		String res = "";
+		Stack<Integer> newStack = new Stack<Integer>();
+		while (!st.empty())
+			newStack.add(st.pop());
+		while (!newStack.empty()) {
+			int re = newStack.pop();
+			st.add(re);
+			res += re + " * ";
+		}
+			
+		if (res.contains(" "))
+			res = res.substring(0, res.length()-2);
+		return res;
+	}
+	private static int lowestDiv(int tal) {
+		int res = -1;
+		if (prim(tal))
+			return 1;
+		for (int i = 2; i < (tal/2)+1; i++) {
+			if (tal % i == 0)
+				return i;
+		}
+		return -1;
+	}
+	
+
+}
---
+++ src/worlde/Wordle.java
@@ -0,0 +1,248 @@
+package worlde;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Wordle {
+
+public static ArrayList<String> ordel(String word, ArrayList<Character> allowed, ArrayList<Character> required, ArrayList<Integer> indices, ArrayList<Character> wildCards) {
+		
+		for (int i = 0; i < required.size(); i++) {
+			if (!allowed.contains(required.get(i)))
+				allowed.add(required.get(i));
+		}
+		for (int i = 0; i < wildCards.size(); i++) {
+			if (!allowed.contains(wildCards.get(i)))
+				allowed.add(wildCards.get(i));
+		}
+		for(int i = 0; i<word.length(); i++) {
+
+		      // access each character
+		      char a = word.charAt(i);
+		      if (!allowed.contains(a) && Character.isLetter(a))
+		    	  allowed.add(a);
+		    }
+		
+		ArrayList<String> res = new ArrayList<String>();
+		
+		
+		
+		
+		
+		ArrayList<String> allStrings0 = ordel2(word, allowed, wildCards);
+		ArrayList<String> allStrings = new ArrayList<String>();
+		if (wildCards.size() > 0) {
+			for (int i = 0; i < allStrings0.size(); i++) {
+				boolean includeThis = true;
+				for (int j = 0; j < wildCards.size(); j++) {
+					CharSequence s2 = "" + wildCards.get(j);
+					if (!allStrings0.get(i).contains(s2))
+						includeThis = false;
+				}
+				if (includeThis)
+					allStrings.add(allStrings0.get(i));
+			}
+				
+		}
+		else
+			allStrings = allStrings0;
+		
+		
+		int generated = 0;
+		for (int i = 0; i < allStrings.size(); i++) {
+			generated++;
+			String s = allStrings.get(i);
+			if (s.equals("ERASE"))
+				System.out.println("BOY!");
+			
+			
+			if (required.size() < 1) {
+				if (!res.contains(s))
+					res.add(s);
+				}
+			else 
+			{
+				boolean containsKeyLetters = true;
+				for (int j = 0; j < required.size(); j++) {
+					int ind = s.indexOf(required.get(j));
+					if (ind == -1)
+						containsKeyLetters = false;
+				}
+				
+				
+				try {
+				//MainFunc.rawPrint("compare " + s.charAt(indices.get(0)) + required.get(0));
+					if (s.equals("CATTY"))
+						System.out.println("CATTT");
+					if (s.charAt(indices.get(0)) == required.get(0)) 
+						containsKeyLetters = false;
+					
+					
+					if (required.size() > 1) {
+						//MainFunc.rawPrint("compare " + s.charAt(indices.get(1)) + required.get(1));
+						if (s.charAt(indices.get(1)) == required.get(1))
+							containsKeyLetters = false;
+					}
+					
+					if (required.size() > 2) {
+						//MainFunc.rawPrint("compare " + s.charAt(indices.get(2)) + required.get(2));
+						if (s.charAt(indices.get(2)) == required.get(2))
+							containsKeyLetters = false;
+					}
+					
+					if (required.size() > 3) {
+						if (s.charAt(indices.get(3)) == required.get(3))
+							containsKeyLetters = false;
+					}
+					if (required.size() > 4) {
+						if (s.charAt(indices.get(4)) == required.get(4))
+							containsKeyLetters = false;
+					}
+					if (required.size() > 5) {
+						if (s.charAt(indices.get(5)) == required.get(5))
+							containsKeyLetters = false;
+					}
+					if (required.size() > 6) {
+						if (s.charAt(indices.get(6)) == required.get(6))
+							containsKeyLetters = false;
+					}
+					if (required.size() > 7) {
+						if (s.charAt(indices.get(7)) == required.get(7))
+							containsKeyLetters = false;
+					}
+				}
+				catch (StringIndexOutOfBoundsException ex)  {
+					System.out.println("failed experiment");
+				}
+				
+				if (containsKeyLetters) {
+					if (!res.contains(s))
+						res.add(s);
+				}
+			}
+			
+			
+					
+			
+		}
+		
+		
+		
+		System.out.println("current word: " + word);
+		System.out.println("allowed : " + allowed);
+		System.out.println("required : " + required);
+		System.out.println("generated " + generated + ", " + res.size() + " viable ones");		
+		
+		
+		
+		
+		return res;
+	
+	}
+public static ArrayList<String> ordel2(String word, ArrayList<Character> allowed, ArrayList<Character> wildCards) {
+	ArrayList<String> res = new ArrayList<String>();
+	ArrayList<String> res2  = new ArrayList<String>();
+	
+	int empty = 0;
+	for (int i = 0; i < word.length(); i++) {
+		if (word.charAt(i) == ' ')
+			empty++;
+	}
+	
+	
+	
+	int letters = allowed.size();
+	
+	//int ind = s.indexOf(" ");
+	//&& res.get(i).indexOf(" ") != -1
+	
+	for (int i = 0; i < allowed.size(); i++) {
+		String s = word.replaceFirst(" ", allowed.get(i).toString());
+		res.add(s);
+	}
+	if (noSpace(res))
+		return res;
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < letters; j++) {
+			String s = res.get(i).replaceFirst(" ", allowed.get(j).toString());
+			res2.add(s);
+		}
+	}
+	if (noSpace(res2))
+		return res2;
+	res.clear();
+	for (int i = 0; i < res2.size(); i++) {
+		for (int j = 0; j < letters; j++) {
+			String s = res2.get(i).replaceFirst(" ", allowed.get(j).toString());
+			res.add(s);
+		}
+	}
+	if (noSpace(res))
+		return res;
+	res2.clear();
+		for (int i = 0; i < res.size(); i++) {
+			for (int j = 0; j < letters; j++) {
+				String s = res.get(i).replaceFirst(" ", allowed.get(j).toString());
+				res2.add(s);
+		}
+	}
+	if (noSpace(res2))
+		return res2;
+	res.clear();
+	for (int i = 0; i < res2.size() && res2.get(i).indexOf(" ") != -1; i++) {
+		for (int j = 0; j < letters; j++) {
+			String s = res2.get(i).replaceFirst(" ", allowed.get(j).toString());
+			res.add(s);
+		}
+	}
+	
+	return res;
+	}
+	private static boolean noSpace(ArrayList<String> l) {
+		return l.get(0).indexOf(" ") == -1;
+	}
+	
+	
+	public static double similarity(String s1, String s2) {
+	    String longer = s1, shorter = s2;
+	    if (s1.length() < s2.length()) { // longer should always have greater length
+	      longer = s2; shorter = s1;
+	    }
+	    int longerLength = longer.length();
+	    if (longerLength == 0) { return 1.0; /* both strings are zero length */ }
+	    /* // If you have Apache Commons Text, you can use it to calculate the edit distance:
+	    LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+	    return (longerLength - levenshteinDistance.apply(longer, shorter)) / (double) longerLength; */
+	    return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
+
+	  }
+	
+	 public static int editDistance(String s1, String s2) {
+		    s1 = s1.toLowerCase();
+		    s2 = s2.toLowerCase();
+
+		    int[] costs = new int[s2.length() + 1];
+		    for (int i = 0; i <= s1.length(); i++) {
+		      int lastValue = i;
+		      for (int j = 0; j <= s2.length(); j++) {
+		        if (i == 0)
+		          costs[j] = j;
+		        else {
+		          if (j > 0) {
+		            int newValue = costs[j - 1];
+		            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+		              newValue = Math.min(Math.min(newValue, lastValue),
+		                  costs[j]) + 1;
+		            costs[j - 1] = lastValue;
+		            lastValue = newValue;
+		          }
+		        }
+		      }
+		      if (i > 0)
+		        costs[s2.length()] = lastValue;
+		    }
+		    return costs[s2.length()];
+		  }
+	
+}
