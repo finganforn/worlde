65eb0ff42811c5ec297bbc7590ec6559d5263453
finganforn <erik.hahne@gmail.com>
2023-09-27 11:08:43 +0200

    operationsSpel

--- src/worlde/MainFunc.java
+++ src/worlde/MainFunc.java
@@ -6,10 +6,14 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Stack;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -25,7 +29,7 @@ public class MainFunc {
 	static String swedishAlphabet = "QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM";
 	static String englishAlphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
 	static String swedishShortcut = "QWRUSDFHJÄZXCB";
-	static String englishShortcut = "QGJZXVB chuf";
+	static String englishShortcut = "QWYFGHJKZXV build";
 	static String ordlig = "QWYFHJKÖÄZXCVM påbud";
 	static String ordlig6 = "QWYÅDHÖZXVB";
 	static String swedishFile = "doesnt matter";
@@ -34,7 +38,7 @@ public class MainFunc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*
+		
 		Random random = new Random();
 		int startP = random.nextInt(10000);
 		//startP = 1950;
@@ -55,7 +59,24 @@ public class MainFunc {
 		}
 		
 		
-		*/
+		
+		//ArrayList<Integer> nums = new ArrayList<Integer>();
+		//nums.add(15); nums.add(6); nums.add(7); nums.add(4); nums.add(2);
+		//operationsGame(nums, 59);
+		
+		
+		for (int hej = 0; hej < 10; hej++) {
+			ArrayList<Integer> generatedProb = generateProb();
+			while (generatedProb == null)
+				generatedProb = generateProb();
+			int probSize = generatedProb.size();
+			int probRes = generatedProb.get(probSize-1);
+			generatedProb.remove(probSize-1);
+			System.out.println("numbers: " + generatedProb + " wanted result: " + probRes);
+			operationsGame(generatedProb, probRes);
+		}
+		
+		
 		File directory = new File("./");
 		String fileP = directory.getAbsolutePath();
 		fileP = fileP.substring(0, fileP.length()-1);
@@ -495,6 +516,160 @@ public class MainFunc {
 				return i;
 		}
 		return -1;
+	}
+	private static String operationsGame(ArrayList<Integer> nums, int res) {
+		ArrayList<Integer> ops = new ArrayList<Integer>();
+		int realPossibilites = (int) Math.pow(4, nums.size()-1);
+		int possibilities = (int) Math.pow(10, nums.size()-1);
+		for (int i = 0; i < possibilities; i++) {
+			String s = Integer.toString(i);
+			boolean validString = true;
+			for (int j = 0; j < s.length(); j++) {
+				char cx = s.charAt(j);
+				if (charToInt(cx) > 3)
+					validString = false;
+			}
+			if (validString)
+				ops.add(i);
+		}
+		//System.out.println("possible solutions " + realPossibilites + " generated: " + ops.size());
+		ArrayList<String> opsS = new ArrayList<String>();
+		for (int i = 0; i < ops.size(); i++) {
+			String s = ops.get(i).toString();
+			while (s.length() != nums.size()-1)
+				s = "0" + s;
+			s = s.replace('0', '+');
+			s = s.replace('1', '-');
+			s = s.replace('2', '*');
+			s = s.replace('3', '/');
+			opsS.add(s);
+		}
+		
+		ArrayList<String> opsS2 = new ArrayList<String>();
+		for (int i = 0; i < opsS.size(); i++) {
+			String r = "";
+			for (int j = 0; j < opsS.get(0).length(); j++) {
+				r += nums.get(j).toString();
+				r += opsS.get(i).charAt(j);
+			}
+			int lastNum = nums.get(nums.size()-1);
+			String lastNumS = Integer.toString(lastNum);
+			r += lastNumS;
+			opsS2.add(r);
+		}
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("js");
+		for (int i = 0; i < opsS2.size(); i++) {
+			String s = opsS2.get(i);
+			
+			
+			//ScriptEngineManager manager = new ScriptEngineManager();
+			//ScriptEngine engine = manager.getEngineByName("js");
+			try {
+				Object result = engine.eval(s);
+				String resMaybe = "" + result;
+				try {
+				int resM = Integer.parseInt(resMaybe);
+				if (resM == res)
+					{
+						System.out.println(s + " = " + resM);
+						return s;
+					}
+				}
+				catch (NumberFormatException ex) {
+					
+				}
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		return "none";
+	}
+	private static int charToInt(char c ) {
+		if (c == '0')
+			return 0;
+		if (c == '1')
+			return 1;
+		if (c == '2')
+			return 2;
+		if (c == '3')
+			return 3;
+		if (c == '4')
+			return 4;
+		if (c == '5')
+			return 5;
+		if (c == '6')
+			return 6;
+		if (c == '7')
+			return 7;
+		if (c == '8')
+			return 8;
+		if (c == '9')
+			return 9;
+		
+		return -1;
+	}
+	
+	
+	private static ArrayList<Integer> generateProb() {
+		
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Random rnd = new Random();		
+		int nums = 0;
+		
+		
+		
+		while (nums < 4)
+			nums = rnd.nextInt(7);
+		for (int i = 0; i < nums; i++)
+		{
+			int term = rnd.nextInt(20);
+			res.add(term);
+		}
+		String probString = "";
+		for (int i = 0; i < nums-1; i++)
+		{
+			probString += res.get(i).toString();
+			int operationInt = rnd.nextInt(4);
+			if (operationInt == 0)
+				probString += "+";
+			if (operationInt == 1)
+				probString += "-";
+			if (operationInt == 2)
+				probString += "*";
+			if (operationInt == 3)
+				probString += "/";
+		}
+		
+		probString += res.get(res.size()-1);
+		
+		
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("js");
+		try {
+			Object result = engine.eval(probString);
+			String result2 = "" + result;
+			try {
+				int resInt = Integer.parseInt(result2);
+				if (resInt < 1)
+					return null;
+				res.add(resInt);
+				return res;
+			}
+			catch (Exception ex) {
+				return null;
+			}
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return res;
 	}
 	
 
